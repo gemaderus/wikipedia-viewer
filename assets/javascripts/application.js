00c5cde0a05f75577e27f0aa807f596e4b78292f
gemaderus <hi@gemaderus.com>
2016-09-22 17:04:08 +0200

    Fine tune js and styles.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,24 +2,31 @@ var $ = require('jquery');
 
 var TEMPLATE = "<li class='Topic-item'><h3 class='Topic-item-title'></h3><p class='Topic-item-text'></p><a href='#' class='Btn Btn-action Btn-link'>Read more</a></li>";
 
+//To search the contents.
+
 $(function() {
   $("#button-search").on("click", function(e) {
     e.preventDefault();
     var searchValue = $("#search-input").val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchValue +"&format=json&callback=?"
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchValue +"&pwpcontinue&format=json&callback=?"
 
     search(url);
   });
 });
 
+//To get the data
+
 function search(url) {
   $.getJSON(url, handleResponse);
 }
+
+//To handle the api
 
 function handleResponse(response) {
   var topicsList = response[1];
   var descList = response[2];
   var urlList = response[3];
+  background();
 
   $('#Topic-list').empty();
 
@@ -33,9 +40,13 @@ function handleResponse(response) {
   });
 }
 
+//To get the title
+
 function updateTitle(nodo, data) {
   nodo.find(".Topic-item-title").html(data);
 }
+
+//To get the text
 
 function updateText(nodo, text) {
   nodo.find(".Topic-item-text").html(text);
@@ -45,3 +56,51 @@ function readMore(nodo, url) {
   nodo.find(".Btn-link").attr("href", url);
 }
 
+function background() {
+  $(".Body").addClass("is-clicked");
+}
+
+//To get the random content
+
+// $(function() {
+//   $("#btn-random").on("click", function(e) {
+//     e.preventDefault();
+//     // var url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&list=random&rnlimit=10&format=json&callback=?"
+
+//     var url ="https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&generator=random&grnnamespace=0&rnlimit=10&callback=?"
+//     random(url);
+//   });
+// })
+
+// function random(url) {
+//   $.getJSON(url, function() {
+//     console.log(url);
+//   });
+// }
+
+// function handleRamdom(response) {
+//   var topicsList = response.query.pages;
+//   // var descList = response[2];
+//   // var urlList = response[3];
+
+//   // $('#Topic-list').empty();
+
+//   // topicsList.forEach(function (topicItem, index) {
+//   //   var nodo = $(TEMPLATE);
+//   //   updateTitle(nodo, topicItem);
+//   //   updateText(nodo, descList[index]);
+//   //   readMore(nodo, urlList[index]);
+
+//     // $('#Topic-list').append(nodo);
+//     Object.keys(response.query);
+// };
+
+// function updateRandomTitle(data) {
+//   var tittle =data.title;
+//   nodo.find(".Topic-item-text").html(tittle);
+// }
+
+// function updateRandomText(nodo, text) {
+//   var description =data.extract;
+//   nodo.find(".Topic-item-text").html(description);
+// }
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -48,24 +48,31 @@
 
 	var TEMPLATE = "<li class='Topic-item'><h3 class='Topic-item-title'></h3><p class='Topic-item-text'></p><a href='#' class='Btn Btn-action Btn-link'>Read more</a></li>";
 
+	//To search the contents.
+
 	$(function() {
 	  $("#button-search").on("click", function(e) {
 	    e.preventDefault();
 	    var searchValue = $("#search-input").val();
-	    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchValue +"&format=json&callback=?"
+	    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchValue +"&pwpcontinue&format=json&callback=?"
 
 	    search(url);
 	  });
 	});
 
+	//To get the data
+
 	function search(url) {
 	  $.getJSON(url, handleResponse);
 	}
+
+	//To handle the api
 
 	function handleResponse(response) {
 	  var topicsList = response[1];
 	  var descList = response[2];
 	  var urlList = response[3];
+	  background();
 
 	  $('#Topic-list').empty();
 
@@ -79,9 +86,13 @@
 	  });
 	}
 
+	//To get the title
+
 	function updateTitle(nodo, data) {
 	  nodo.find(".Topic-item-title").html(data);
 	}
+
+	//To get the text
 
 	function updateText(nodo, text) {
 	  nodo.find(".Topic-item-text").html(text);
@@ -91,6 +102,54 @@
 	  nodo.find(".Btn-link").attr("href", url);
 	}
 
+	function background() {
+	  $(".Body").addClass("is-clicked");
+	}
+
+	//To get the random content
+
+	// $(function() {
+	//   $("#btn-random").on("click", function(e) {
+	//     e.preventDefault();
+	//     // var url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&list=random&rnlimit=10&format=json&callback=?"
+
+	//     var url ="https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&generator=random&grnnamespace=0&rnlimit=10&callback=?"
+	//     random(url);
+	//   });
+	// })
+
+	// function random(url) {
+	//   $.getJSON(url, function() {
+	//     console.log(url);
+	//   });
+	// }
+
+	// function handleRamdom(response) {
+	//   var topicsList = response.query.pages;
+	//   // var descList = response[2];
+	//   // var urlList = response[3];
+
+	//   // $('#Topic-list').empty();
+
+	//   // topicsList.forEach(function (topicItem, index) {
+	//   //   var nodo = $(TEMPLATE);
+	//   //   updateTitle(nodo, topicItem);
+	//   //   updateText(nodo, descList[index]);
+	//   //   readMore(nodo, urlList[index]);
+
+	//     // $('#Topic-list').append(nodo);
+	//     Object.keys(response.query);
+	// };
+
+	// function updateRandomTitle(data) {
+	//   var tittle =data.title;
+	//   nodo.find(".Topic-item-text").html(tittle);
+	// }
+
+	// function updateRandomText(nodo, text) {
+	//   var description =data.extract;
+	//   nodo.find(".Topic-item-text").html(description);
+	// }
 
 
 /***/ },
